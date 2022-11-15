9ab9c98011d9840d4f2725a5fd00ced57de845ad
JP <85654561+jp524@users.noreply.github.com>
2022-11-14 19:56:55 -0500

    Build #show pages for Users and Events

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root "events#index"
 
-  resources :events, only: [:index]
+  resources :events, only: [:index, :show]
+  resources :users, only: [:show]
 end
