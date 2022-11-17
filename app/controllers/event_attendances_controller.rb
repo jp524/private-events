76f376bf47b3446a6ff3ce12cc715048f6806eb5
JP <85654561+jp524@users.noreply.github.com>
2022-11-16 19:03:08 -0500

    Add flash to EventAttendances#create action

--- app/controllers/event_attendances_controller.rb
+++ app/controllers/event_attendances_controller.rb
@@ -1,6 +1,7 @@
 class EventAttendancesController < ApplicationController
   def create
     @event_attendance = EventAttendance.create(event_attendance_params)
+    flash[:success] = 'You successfully registered for this event!'
     redirect_to root_path
   end
 
