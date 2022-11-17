711899e332879063dacc90a0b9ffe02a54cfbf60
JP <85654561+jp524@users.noreply.github.com>
2022-11-16 20:42:51 -0500

    Separe past and future events on Event#index by using a class method

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,12 @@ class Event < ApplicationRecord
   belongs_to :creator, class_name: "User"
   has_many :event_attendances, foreign_key: "event_id"
   has_many :attendees, through: :event_attendances
+
+  def self.past
+    Event.where(date: (..Time.now))
+  end
+
+  def self.future
+    Event.where(date: (Time.now..))
+  end
 end
